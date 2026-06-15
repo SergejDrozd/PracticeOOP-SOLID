c89fe8cb91f253ff259757e509aa8dcc5d858923
Sergey <sergejdrozd06@gmail.com>
2026-06-15 09:56:41 +0300

    Task4:Inheritance and polymorphism

---
+++ FoufthTask/Program.cs
@@ -0,0 +1,57 @@
+﻿abstract class Transport
+{
+    public double Speed;
+    public abstract double CalculateTime(double distance);
+}
+class Truck : Transport
+{
+    public Truck()
+    {
+        Speed = 80;
+    }
+    public override double CalculateTime(double distance)
+    {
+        return distance / Speed;
+    }
+}
+class CargoPlane : Transport
+{
+    public CargoPlane()
+    {
+        Speed = 800;
+    }
+    public override double CalculateTime(double distance)
+    {
+        return distance / Speed + 2;
+    }
+}
+
+class Train : Transport
+{
+    public Train()
+    {
+        Speed = 120;
+    }
+    public override double CalculateTime(double distance)
+    {
+        return distance / Speed;
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        var Truck1 = new Truck();
+        var Plane1 = new CargoPlane();
+        var Train1 = new Train();
+        double distance = 2400;
+        double TruckTime, TrainTime, PlaneTime;
+        TruckTime = Truck1.CalculateTime(distance);
+        PlaneTime = Plane1.CalculateTime(distance);
+        TrainTime = Train1.CalculateTime(distance);
+        Console.WriteLine(TruckTime);
+        Console.WriteLine(PlaneTime);
+        Console.WriteLine(TrainTime);
+    }
+}
